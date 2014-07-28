c63cc269fc0499b3ccbecf060d49cccf45c948e1
Boris Belozovsky <boris.belozovsky@hp.com>
2014-07-28 17:18:18 +0300

    #Internal: small change

--- src/main/java/com/hp/devops/MainClass.java
+++ src/main/java/com/hp/devops/MainClass.java
@@ -9,7 +9,7 @@ public class MainClass {
 
     public static void main(String[] args) {
 
-        System.out.println("Hello world");
+        System.out.println("Hello, world!");
     }
 
     public float divide(int dividend, int divider){
